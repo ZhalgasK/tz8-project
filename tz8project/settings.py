4e022a101dea89fb535a26b8e9c681547e09da4e
Akhan-Nurdauletov <77663086+Akhan-Nurdauletov@users.noreply.github.com>
2021-06-11 19:26:47 +0600

    back to dbsqlite3

--- tz8project/settings.py
+++ tz8project/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'tz8project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tz8projectdb',
-        'USER': 'postgres',
-        'PASSWORD': 'Akhan2020',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
